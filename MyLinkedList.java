ade3ab2e0c26288f97025091e21bcafb4c52c62a
R KARTHIK <karthikr44@gmail.com>
2016-11-05 22:30:46 -0700

    Added LinkedList.java

---
+++ MyLinkedList.java
@@ -0,0 +1,53 @@
+class MyLinkedList {
+ int[] arr = new int[10]; 
+ int size = 0; 
+ 
+ public int size() {
+  return size;
+ }
+ 
+ public void set(int index, int item) {
+  if(index <0 || index >= size) {
+   throw new ArrayIndexOutOfBoundsException(index); 
+  }
+  arr[index] = item;
+ }
+ 
+ public void append(int item) {
+  checkCapacity(); 
+  arr[size] = item;
+  size++; 
+ }
+ 
+ private void checkCapacity() {
+  if (arr.length == size) {
+   int[] copy = new int[size*2]; 
+   System.arraycopy(arr, 0, copy, 0, size); 
+   arr = copy; 
+  }
+ }
+ 
+ public int get(int index) {
+  if(index <0 || index >= size) {
+   throw new ArrayIndexOutOfBoundsException(index); 
+  }
+  return arr[index]; 
+ }
+ 
+}
+
+
+public class HelloWorld{
+
+     public static void main(String []args){
+        System.out.println("Hello World");
+        
+        MyLinkedList li = new MyLinkedList(); 
+        
+        li.append(10); 
+        li.append(20); 
+        
+        System.out.println(li.get(0)); 
+       
+     }
+}
